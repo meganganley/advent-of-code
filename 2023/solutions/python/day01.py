e0feffa5f64eb5ab5c1fb9186c162eaa26c5890d
Megan <mgn.gnly@gmail.com>
2023-12-01 18:27:44 +1300

    Refactor part1 solution

--- 2023/solutions/python/day01.py
+++ 2023/solutions/python/day01.py
@@ -1,4 +1,4 @@
-
+import re
 
 with open('C:\\Users\\Megan\\Documents\\Projects\\advent-of-code\\2023\\input\\day01_input.txt') as f:
     lines = f.readlines()
@@ -7,22 +7,19 @@ calibration_values = []
 
 numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'] # no zero? 
 
+
 for line in lines:
     calibration_value = ''
-    for char in line:
-        if char.isnumeric():
-            calibration_value += char
-            break
-        else:
-            continue
 
-    for char in line[::-1]:
-        if char.isnumeric():
-            calibration_value += char
-            break
-        else:
-            continue
-
+    digit = re.search('\d', line)
+    calibration_value += digit.group(0)
+    
+    digit = re.search('\d', line[::-1])
+    calibration_value += digit.group(0)
+    
     calibration_values.append(int(calibration_value))
 
 print(sum(calibration_values)) # Part 1 - 54081
+
+
+ # Part 1 - 54081
