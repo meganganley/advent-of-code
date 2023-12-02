2569a3599c728d8566dd98acc5f750dc96ebeda3
Megan <mgn.gnly@gmail.com>
2023-12-02 18:49:31 +1300

    Solution for part 1

--- 2023/solutions/python/day02.py
+++ 2023/solutions/python/day02.py
@@ -1,37 +1,40 @@
-import re
+class Game:
+    is_possible = 1
 
-def create_subsets(all_subsets):
-    subsets_list = all_subsets.split(';')
+    def __init__(self, line):
+        game_info = line.split(':')
+        self.id = int(game_info[0].split(' ')[1])
+        self.subsets = self.create_subsets(game_info[1])
 
-    parsed_subsets = []
+    def create_subsets(self, all_subsets):
+        subsets_list = all_subsets.split(';')
+        parsed_subsets = []
 
-    for subset_string in subsets_list:
-        subset = Subset(0,0,0)
-        colours = subset_string.split(',')
-        for colour in colours:
-            if 'red' in colour:
-                subset.red = colour.split()[0]
-            if 'blue' in colour:
-                subset.blue = colour.split()[0]
-            if 'green' in colour:
-                subset.green = colour.split()[0]
-        parsed_subsets.append(subset)
+        for subset_string in subsets_list:
+            subset = Subset(0,0,0)
+            colours = subset_string.split(',')
+            for colour in colours:
+                if 'red' in colour:
+                    subset.red = int(colour.split()[0])
+                    if subset.red > max_red:
+                        self.is_possible = 0
+                if 'blue' in colour:
+                    subset.blue = int(colour.split()[0])
+                    if subset.blue > max_blue:
+                        self.is_possible = 0
+                if 'green' in colour:
+                    subset.green = int(colour.split()[0])
+                    if subset.green > max_green:
+                        self.is_possible = 0
+            parsed_subsets.append(subset)
 
-    return parsed_subsets
+        return parsed_subsets
+
 
 class Subset:
     def __init__(self, red, blue, green):
         self.red = red
         self.blue = blue
-        self.green = green
-        
-    def set_red(self, red):
-        self.red = red
-        
-    def set_blue(self, blue):
-        self.blue = blue
-        
-    def set_green(self, green):
         self.green = green
         
     def __str__(self):
@@ -42,28 +45,21 @@ class Subset:
 with open('C:\\Users\\Megan\\Documents\\Projects\\advent-of-code\\2023\\input\\day02_input.txt') as f:
     lines = f.readlines()
 
-games = {}
 
 max_red = 12
-max_green = 13 
 max_blue = 14
+max_green = 13 
 
-count = 0
+total = 0
 
 for line in lines:
-    game_info = line.split(':')
-    game_id = game_info[0].split(' ')[1]
-    subsets = create_subsets(game_info[1])
-    print('Game: {}'.format(game_id)) 
-
-    for subset in subsets:
-        print(subset)
-
-    count += 1 
-    if count > 4:
-        break
+    game = Game(line)
+    
+    if game.is_possible:
+        total += game.id
 
 
- # Part 1 - 54081
- # Part 2 - 54649
+print(total)
+
+ # Part 1 - 2685
 
