073f987ccb741ce9308aa4f9efc6d67f802d66b6
Megan <mgn.gnly@gmail.com>
2023-12-02 18:58:25 +1300

    Add solution for day2 part2

--- 2023/solutions/python/day02.py
+++ 2023/solutions/python/day02.py
@@ -1,5 +1,8 @@
 class Game:
     is_possible = 1
+    min_red = 0
+    min_blue = 0
+    min_green = 0
 
     def __init__(self, line):
         game_info = line.split(':')
@@ -18,17 +21,26 @@ class Game:
                     subset.red = int(colour.split()[0])
                     if subset.red > max_red:
                         self.is_possible = 0
+                    if subset.red > self.min_red:
+                        self.min_red = subset.red
                 if 'blue' in colour:
                     subset.blue = int(colour.split()[0])
                     if subset.blue > max_blue:
                         self.is_possible = 0
+                    if subset.blue > self.min_blue:
+                        self.min_blue = subset.blue
                 if 'green' in colour:
                     subset.green = int(colour.split()[0])
                     if subset.green > max_green:
                         self.is_possible = 0
+                    if subset.green > self.min_green:
+                        self.min_green = subset.green
             parsed_subsets.append(subset)
 
         return parsed_subsets
+
+    def get_power(self):
+        return self.min_red * self.min_blue * self.min_green
 
 
 class Subset:
@@ -39,7 +51,6 @@ class Subset:
         
     def __str__(self):
         return "red: {}, blue: {}, green: {}".format(self.red, self.blue, self.green)
-
     
 
 with open('C:\\Users\\Megan\\Documents\\Projects\\advent-of-code\\2023\\input\\day02_input.txt') as f:
@@ -50,16 +61,23 @@ max_red = 12
 max_blue = 14
 max_green = 13 
 
-total = 0
+total_possible = 0
+total_power = 0
 
 for line in lines:
     game = Game(line)
     
     if game.is_possible:
-        total += game.id
+        total_possible += game.id
+    
+    print("Min red: {}, min blue: {}, min green: {}".format(game.min_red, game.min_blue, game.min_green))
+    print("Power: {}".format(game.get_power()))
 
+    total_power += game.get_power()
 
-print(total)
+print(total_possible)
+print(total_power)
 
  # Part 1 - 2685
+ # Part 2 - 83707
 
