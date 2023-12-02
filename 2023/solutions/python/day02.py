f863e892e3fb24598391b8917ed119ab03570a60
Megan <mgn.gnly@gmail.com>
2023-12-02 19:45:30 +1300

    Formatting fix, change file read

--- 2023/solutions/python/day02.py
+++ 2023/solutions/python/day02.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 class Game:
     is_possible = 1
     min_red = 0
@@ -23,18 +25,21 @@ class Game:
                         self.is_possible = 0
                     if subset.red > self.min_red:
                         self.min_red = subset.red
+
                 if 'blue' in colour:
                     subset.blue = int(colour.split()[0])
                     if subset.blue > max_blue:
                         self.is_possible = 0
                     if subset.blue > self.min_blue:
                         self.min_blue = subset.blue
+
                 if 'green' in colour:
                     subset.green = int(colour.split()[0])
                     if subset.green > max_green:
                         self.is_possible = 0
                     if subset.green > self.min_green:
                         self.min_green = subset.green
+                        
             parsed_subsets.append(subset)
 
         return parsed_subsets
@@ -52,11 +57,14 @@ class Subset:
     def __str__(self):
         return "red: {}, blue: {}, green: {}".format(self.red, self.blue, self.green)
     
+### Read input
+base_path = Path(__file__).parent
+path = (base_path / "../../input/day02_input.txt").resolve()
 
-with open('C:\\Users\\Megan\\Documents\\Projects\\advent-of-code\\2023\\input\\day02_input.txt') as f:
+with open(path) as f:
     lines = f.readlines()
 
-
+### Set up problem
 max_red = 12
 max_blue = 14
 max_green = 13 
@@ -64,6 +72,7 @@ max_green = 13
 total_possible = 0
 total_power = 0
 
+## Find solution
 for line in lines:
     game = Game(line)
     
