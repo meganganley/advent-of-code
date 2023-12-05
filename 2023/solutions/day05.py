c9ee648235d5a7c596e9e91f24a5fe81970fc1e5
Megan <mgn.gnly@gmail.com>
2023-12-05 19:52:04 +1300

    Solution for 2023 day5 part1

--- 2023/solutions/day05.py
+++ 2023/solutions/day05.py
@@ -29,30 +29,60 @@ for line in lines:
     else:
         maps[current_map] = [line.strip()]
 
+print(maps)
 
-list_of_maps = []
+'''
+
+lists_and_offsets = []
 for key, value in maps.items():
-    temp_dict = {}
+    temp_list = []
     for r in value:
         values = r.split() 
         values = [eval(x) for x in values]
         count = 0
-        for i in range(values[1], values[1]+values[2]):
-            temp_dict[i] = values[0]+count
-            count += 1
-    list_of_maps.append(temp_dict)
-    
+        offset = values[0] - values[1] # 98
+        temp_list = []
+        temp_list.extend(range(values[1], values[1]+values[2]))
+
+        #temp_list = []
+        #for i in range(values[1], values[1]+values[2]):
+        #    temp_dict[i] = values[0]+count
+        #    count += 1
+        #lists_and_offsets.append((temp_list, offset))
+    break
+'''
+
 locations = []
 
+# each seed from first line of input
 for seed in seeds:
     lookup = seed
-    for map in list_of_maps:
-        if lookup in map.keys():
-            lookup = map[lookup]
+    # each set of maps, e.g. 'seed-to-soil'
+    for key in maps:  
+        lookup_found = False      
+        # there are multiple options for each map, check until you find at least one that fits
+        for map in maps[key]:
+            #print(map)
+            if lookup_found:
+                break
+
+            values = map.split() 
+            values = [eval(x) for x in values]
+            #print('lookup checking: ' + str(lookup) + ' : ' + str(values[1]) + ' -> ' + str(values[1] + values[2]) + ' for ' + key)
+
+            if lookup >= values[1] and lookup < values[1] + values[2]: 
+                print('lookup found: ' + str(lookup) + ' -> ' + str(lookup - (values[1] - values[0])) + ' for ' + key)
+                lookup = lookup - (values[1] - values[0])
+                lookup_found = True
+                break
+    
+    print('location found ' + str(lookup) )
     locations.append(lookup)
     
 
-print(min(locations))
+print(min(locations)) # 165788812
+    
+
 
 
 
