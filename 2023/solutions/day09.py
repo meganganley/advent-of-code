6b302c520897a961f6dc00ce4bdd3955cd42c524
Megan <mgn.gnly@gmail.com>
2023-12-09 19:29:45 +1300

    2023 day9 part2

--- 2023/solutions/day09.py
+++ 2023/solutions/day09.py
@@ -8,7 +8,8 @@ path = (base_path / "../input/day09_input.txt").resolve()
 with open(path) as f:
     lines = f.readlines()
 
-result = 0
+next_result = 0
+previous_result = 0
 result_list = []
 
 for line in lines:
@@ -17,35 +18,26 @@ for line in lines:
     diff = []
 
     last_diffs = [seq[-1]]
-
-    if sum(seq) == seq[0]*len(seq):
-        result += seq[len(seq) - 1] + (seq[len(seq) - 1] - seq[len(seq) - 2])
-        continue
+    first_diffs = [seq[0]]
 
     while not all(ele == 0 for ele in seq):
         for i in range(1, len(seq)):
             diff.append(seq[i] - seq[i-1])
         last_diffs.insert(0,(seq[len(seq) - 1] - seq[len(seq) - 2]))
-        #print(seq)
+        first_diffs.insert(0,(seq[1] - seq[0]))
         seq = diff
         diff = []
-
     
-    #next = last_diffs[0]
     next_diffs = [last_diffs[0]]
     for i in range(1, len(last_diffs)):
         next_diffs.append(last_diffs[i]+next_diffs[i-1])
 
-    result += next_diffs[-1]
-    result_list.append(next_diffs[-1])
+    previous_diffs = [first_diffs[0]]
+    for i in range(1, len(first_diffs)):
+        previous_diffs.append(first_diffs[i]-previous_diffs[i-1])
 
-    if ' 3250965' in line:
-        print(last_diffs)
-        print(next_diffs)
+    next_result += next_diffs[-1]
+    previous_result += previous_diffs[-1]
 
-    #print(last_diffs)
-    #print(next_diffs)
-print(result)
-#for i in result_list:
-    #print(i)
-#print(result_list)
+print(next_result) # 1993300041
+print(previous_result) # 1038
