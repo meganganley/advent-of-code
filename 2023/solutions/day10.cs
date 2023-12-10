b35463601091665f06720ade41e827d08669a07b
Megan <mgn.gnly@gmail.com>
2023-12-10 21:58:14 +1300

    2023 Day10 part1

--- 2023/solutions/day10.cs
+++ 2023/solutions/day10.cs
@@ -15,8 +15,8 @@ namespace solutions
     {
         public static void Run()
         {
-            //List<string> lines = Helper.get_input("day10_input.txt");
-            List<string> lines = Helper.get_input("day10_sample_input.txt");
+            List<string> lines = Helper.get_input("day10_input.txt");
+            //List<string> lines = Helper.get_input("day10_sample_input.txt");
 
             char[] pipes = ['|', '-', 'L', 'J', '7', 'F'];
    
@@ -28,6 +28,15 @@ namespace solutions
 
             char[,] grid = CreateEmptyGrid(lines);
 
+            for (int x = 0; x < numCols; x++)
+            {
+                for (int y = 0; y < numRows; y++)
+                {
+                    Console.Write(grid[x, y]);
+                }
+                Console.WriteLine();
+            }
+
             Point start = new Point(0,0);
 
             List<Point> leftLoop = new List<Point>();
@@ -35,14 +44,14 @@ namespace solutions
             List<Point> usedPoints = new List<Point>();
 
             // populate with input
-            for (int i = 1; i <= numFilledRows; i++)
+            for (int y = 1; y <= numFilledRows; y++)
             {
-                for (int j = 1; j <= numFilledCols; j++)
+                for (int x = 1; x <= numFilledCols; x++)
                 {
-                    grid[i, j] = (char)lines[i - 1][j - 1];
-                    if (grid[i,j] == 'S')
+                    grid[x, y] = lines[y - 1][x - 1];
+                    if (grid[x, y] == 'S')
                     {
-                        start = new Point(j, i);
+                        start = new Point(x, y);
                         usedPoints.Add(start);
                     }
                 }
@@ -53,28 +62,28 @@ namespace solutions
             {
                 for (int j = -1; j <= 1; j++)
                 {
-                    if(pipes.Contains(grid[start.X+i, start.Y + j]))
+                    if(pipes.Contains(grid[start.X+j, start.Y+i]))
                     {
                         if ( i == 0 && j == 0 )
                         {
                             continue;
                         }
-                        char symbol = grid[start.X + i, start.Y + j];
+                        char symbol = grid[start.X+j, start.Y+i];
          
-                        if (!isValidPipe(symbol, i, j))
+                        if (!isValidNextPipe(symbol, j, i))
                         {
                             continue;
                         }
 
                         if (!leftLoop.Any())
                         {
-                            Point leftStart = new Point(start.X + i, start.Y + j);
+                            Point leftStart = new Point(start.X+j, start.Y+i);
                             leftLoop.Add(leftStart);
                             usedPoints.Add(leftStart);
                         }
                         else
                         {
-                            Point rightStart = new Point(start.X + i, start.Y + j);
+                            Point rightStart = new Point(start.X+j, start.Y+i);
                             rightLoop.Add(rightStart);
                             usedPoints.Add(rightStart);
                         }
@@ -88,10 +97,6 @@ namespace solutions
             Point curr = new Point(0,0);
             while (keepLooping)
             {
-                if (forceStop == 13)
-                {
-                    Console.WriteLine('a');
-                }
                 // do left loop
                 curr = leftLoop[forceStop];
 
@@ -114,12 +119,29 @@ namespace solutions
                 usedPoints.Add(next);
 
                 forceStop++;
-                /*
-                if(forceStop > 10)
+                
+                /*if(forceStop > 10)
                 {
                     keepLooping = false;
                 }*/
 
+            }
+
+            int tilesEnclosed = 0;
+
+            for (int y = 1; y <= numFilledRows; y++)
+            {
+                for (int x = 1; x <= numFilledCols; x++)
+                {
+                    // part of the pipe - don't count for enclosed
+                    if (usedPoints.Contains(new Point(x, y)))
+                    {
+                        continue;
+                    } else if (IsPointInPolygon4(usedPoints.ToArray(), new Point(x, y)))
+                    {
+                        tilesEnclosed++;
+                    }
+                }
             }
 
             /*
@@ -134,18 +156,103 @@ namespace solutions
             }*/
 
             // print grid
-            for (int i = 0; i < numRows; i++)
+            /*
+            
+
+            for (int y = 0; y < numRows; y++)
             {
-                for (int j = 0; j < numCols; j++)
+                for (int x = 0; x < numCols; x++)
                 {
-                    Console.Write(grid[i, j]);
+                    Console.Write(grid[x, y]);
                 }
                 Console.WriteLine();
-            }    
+            }  */
 
-            Console.WriteLine(Math.Max(rightLoop.Count, leftLoop.Count));    // Part 1 - 251029473
-            Console.WriteLine(0);    // Part 2 - 251003917
+            Console.WriteLine(Math.Max(rightLoop.Count, leftLoop.Count));    // Part 1 - 6640
+            Console.WriteLine(tilesEnclosed);    // Part 2 - 251003917
 
+        }
+
+        public static bool IsPointInPolygon4(Point[] polygon, Point testPoint)
+        {
+            bool result = false;
+            int j = polygon.Length - 1;
+            for (int i = 0; i < polygon.Length; i++)
+            {
+                if (polygon[i].Y < testPoint.Y && polygon[j].Y >= testPoint.Y ||
+                    polygon[j].Y < testPoint.Y && polygon[i].Y >= testPoint.Y)
+                {
+                    if (polygon[i].X + (testPoint.Y - polygon[i].Y) /
+                       (polygon[j].Y - polygon[i].Y) *
+                       (polygon[j].X - polygon[i].X) < testPoint.X)
+                    {
+                        result = !result;
+                    }
+                }
+                j = i;
+            }
+            return result;
+        }
+
+        public static bool IsInPolygon(Point[] poly, Point p)
+        {
+            Point p1, p2;
+            bool inside = false;
+
+            if (poly.Length < 3)
+            {
+                return inside;
+            }
+
+            var oldPoint = new Point(
+                poly[poly.Length - 1].X, poly[poly.Length - 1].Y);
+
+            for (int i = 0; i < poly.Length; i++)
+            {
+                var newPoint = new Point(poly[i].X, poly[i].Y);
+
+                if (newPoint.X > oldPoint.X)
+                {
+                    p1 = oldPoint;
+                    p2 = newPoint;
+                }
+                else
+                {
+                    p1 = newPoint;
+                    p2 = oldPoint;
+                }
+
+                if ((newPoint.X < p.X) == (p.X <= oldPoint.X)
+                    && (p.Y - (long)p1.Y) * (p2.X - p1.X)
+                    < (p2.Y - (long)p1.Y) * (p.X - p1.X))
+                {
+                    inside = !inside;
+                }
+
+                oldPoint = newPoint;
+            }
+
+            return inside;
+        }
+
+        public static bool isTileEnclosed(List<Point> usedPoints, int x, int y)
+        {
+            bool isEnclosed = true;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if ( i == 0 && j == 0)
+                    {
+                        continue;
+                    }
+                    if (!usedPoints.Contains(new Point(x+j, y+i)))
+                    {
+                        isEnclosed = false;
+                    }
+                }
+            }
+            return isEnclosed;
         }
 
         public static Point getNextPipe(char[,] grid, Point curr, char[] pipes, List<Point> usedPoints)
@@ -163,32 +270,12 @@ namespace solutions
                         continue;
                     }
 
-                    if (currSymbol == '|' && j != 0)
-                    {
-                        continue;
-                    }
-                    if (currSymbol == '-' && i != 0)
-                    {
-                        continue;
-                    }
-                    if (currSymbol == 'L' && ((j != 0 && i != -1) && (j != 1 && i != 0)))
-                    {
-                        continue;
-                    }
-                    if (currSymbol == 'J' && ((j != 1 && i != 0) && (j != 0 && i != 1)))
-                    {
-                        continue;
-                    }
-                    if (currSymbol == '7' && ((j != -1 && i != 0) && (j != 0 && i != 1)))
-                    {
-                        continue;
-                    }
-                    if (currSymbol == 'F' && ((j != 0 && i != -1) && (j != -1 && i != 0)))
-                    {
+                    // Valid next index
+                    if (!isValidNextIndex(currSymbol, j, i)){
                         continue;
                     }
 
-                    coord = new Point(curr.X + i, curr.Y + j);
+                    coord = new Point(curr.X+j, curr.Y+i);
                     if (pipes.Contains(grid[coord.X, coord.Y]))
                     {
                         if (i == 0 && j == 0)
@@ -198,7 +285,7 @@ namespace solutions
                         }
                         char newSymbol = grid[coord.X, coord.Y];
 
-                        if (!isValidPipe(newSymbol, i, j))
+                        if (!isValidNextPipe(newSymbol, j, i))
                         {
                             coord = new Point(-1, -1);
                             continue;
@@ -208,8 +295,6 @@ namespace solutions
                             coord = new Point(-1, -1);
                             continue;
                         }
-                        //rightLoop.Add(coord);
-                        //usedPoints.Add(coord);
                         return coord;
                     }
                 }
@@ -217,30 +302,59 @@ namespace solutions
             return new Point(-1, -1);
         }
 
-        public static bool isValidPipe(char symbol, int x, int y)
+        public static bool isValidNextPipe(char nextSymbol, int x, int y)
         {
             bool isValid = false;
-            if (symbol == '|' && y == 0)
+            if (nextSymbol == '|' && x == 0 && (y == 1 || y == -1))
             {
                 isValid = true;
             }
-            if (symbol == '-' && x == 0)
+            if (nextSymbol == '-' && y == 0 && (x == 1 || x == -1))
+            {
+                isValid = true;
+            }
+            if (nextSymbol == 'L' && ((x == -1 && y == 0) || (x == 0 && y == 1)))
+            {
+                isValid = true;
+            }
+            if (nextSymbol == 'J' && ((x == 0 && y == 1) || (x == 1 && y == 0)))
+            {
+                isValid = true;
+            }
+            if (nextSymbol == '7' && ((x == 0 && y == -1) || (x == 1 && y == 0)))
+            {
+                isValid = true;
+            }
+            if (nextSymbol == 'F' && ((x == -1 && y == 0) || (x == 0 && y == -1)))
+            {
+                isValid = true;
+            }
+            return isValid;
+        }
+        public static bool isValidNextIndex(char currSymbol, int x, int y)
+        {
+            bool isValid = false;
+            if (currSymbol == '|' && x == 0 && (y == 1 || y == -1))
+            {
+                isValid = true;
+            }
+            if (currSymbol == '-' && y == 0 && ( x == 1 || x == -1 ))
+            {
+                isValid = true;
+            }
+            if (currSymbol == 'L' && ((x == 0 && y == -1) || (x == 1 && y == 0)))
+            {
+                isValid = true;
+            }
+            if (currSymbol == 'J' && ((x == -1 && y == 0) || (x == 0 && y == -1)))
             {
                 isValid = true; 
             }
-            if (symbol == 'L' && ((x == 0 && y == -1) || (x == 1 && y == 0)))
+            if (currSymbol == '7' && ((x == -1 && y == 0) || (x == 0 && y == 1)))
             {
                 isValid = true;
             }
-            if (symbol == 'J' && ((x == 1 && y == 0) || (x == 0 && y == 1)))
-            {
-                isValid = true;
-            }
-            if (symbol == '7' && ((x == -1 && y == 0) || (x == 0 && y == 1)))
-            {
-                isValid = true;
-            }
-            if (symbol == 'F' && ((x == 0 && y == -1) || (x == -1 && y == 0)))
+            if (currSymbol == 'F' && ((x == 0 && y == 1) || (x == 1 && y == 0)))
             {
                 isValid = true;
             }
@@ -259,15 +373,16 @@ namespace solutions
             for (int i = 0; i < numRows; i++)
             {
                 grid[i, 0] = '.';
-                grid[i, numRows - 1] = '.';
+                grid[i, numCols - 1] = '.';
             }
 
             for (int i = 0; i < numCols; i++)
             {
                 grid[0, i] = '.';
-                grid[numCols - 1, i] = '.';
+                grid[numRows - 1, i] = '.';
             }
 
+            //grid[x, y] = lines[y - 1][x - 1];
             return grid;
         }
     }
